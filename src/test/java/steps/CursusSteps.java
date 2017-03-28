a76a42aadc55f6f5a5fed0a16032e2da04164254
TEST-PC\Tester <robertvanbuiten@gmail.com>
2017-03-28 14:54:52 +0200

    test

---
+++ src/test/java/steps/CursusSteps.java
@@ -0,0 +1,44 @@
+package steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.nl.Als;
+import cucumber.api.java.nl.Dan;
+import cucumber.api.java.nl.En;
+
+/**
+ * Created by Tester on 3/28/2017.
+ */
+public class CursusSteps {
+
+
+
+    @Als("^ik een accountgegevens invul voor \"([^\"]*)\" \"([^\"]*)\", \"([^\"]*)\" en \"([^\"]*)\" met een standaard adres$")
+    public void ikEenAccountgegevensInvulVoorEnMetEenStandaardAdres(String arg0, String arg1, String arg2, String arg3) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @En("^het password met “test(\\d+)”$")
+    public void hetPasswordMetTest(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @En("^als ik op de knop ‘aanmaken' klik$")
+    public void alsIkOpDeKnopAanmakenKlik() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Dan("^komen mijn gegevens in de database$")
+    public void komenMijnGegevensInDeDatabase() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @En("^kan ik inloggen$")
+    public void kanIkInloggen() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
--- src/test/resources/features/Aanmaken account.feature
+++ src/test/resources/features/Aanmaken account.feature
@@ -1,15 +1,15 @@
 #language:nl
 
-  Functionaliteit: Account aanmaken op de webshop
+Functionaliteit: Account aanmaken op de webshop
 
-    Scenario: gebruiker wil ik een account aan kunnen maken
+  Scenario: gebruiker wil ik een account aan kunnen maken
 
-      Gegeven: het scherm ‘account aanmaken’ zichtbaar is
+    Gegeven: het scherm ‘account aanmaken’ zichtbaar is
 
-      Als ik het account in vul met “John”
-        En het password met “test123”
-        En als ik op de knop ‘aanmaken' klik
+    Als ik een accountgegevens invul voor "John" "tester", "24-08-1978" en "test@test.nl" met een standaard adres
+    En het password met “test123”
+    En als ik op de knop ‘aanmaken' klik
 
-      Dan komen mijn gegevens in de database
-        En kan ik inloggen
+    Dan komen mijn gegevens in de database
+    En kan ik inloggen
 
